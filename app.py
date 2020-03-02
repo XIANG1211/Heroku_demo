58e138312fe6136dd2e26ed235cf2316dad81034
unknown <as104510@gmail.com>
2020-03-02 18:07:11 +0800

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
-import pickle as p
 import json
-import matplotlib.pyplot as plt
 from PIL import Image
-import os
-import time
 from keras.models import load_model
 
 app = Flask(__name__)
@@ -20,4 +16,4 @@ def makecalc():
 
 if __name__ == '__main__':
     model=load_model("models/cnn.h5")
-    app.run(debug=True, host='predictdigits.herokuapp.com')
+    app.run(debug=True, host='havefunai.herokuapp.com')
