84531e328ceef3e665f5c1c2c9ed3e138a3ac031
XIANG1211 <59681305+XIANG1211@users.noreply.github.com>
2020-03-05 09:49:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app.config['MAX_CONTENT_LENGTH'] = 1600 * 1024 * 1024
 model=load_model("./models/cnn.h5")
 @app.route('/api', methods=['POST', 'GET'])
 def makecalc():
-  
-    data = request.get_json(force=True)
-    data=np.array(data["test"])
-    prediction =model.predict(data)
-    return Response(json.dumps(int(np.argmax(prediction))),  mimetype='application/json')
+  if request.method == "POST":
+      data = request.get_json(force=True)
+      data=np.array(data["test"])
+      prediction =model.predict(data)
+      return Response(json.dumps(int(np.argmax(prediction))),  mimetype='application/json')
    #return json.dumps(data)
 if __name__ == '__main__':
     app.run()
