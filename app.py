31979532e97b6864bd70d27e60a4294f2b846751
XIANG1211 <59681305+XIANG1211@users.noreply.github.com>
2020-03-05 11:14:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app.config['MAX_CONTENT_LENGTH'] = 1600 * 1024 * 1024
 model=load_model("./models/cnn.h5")
 @app.route('/api', methods=['POST', 'GET'])
 def makecalc():
-    #data = request.get_json(force=True)
-    #data=np.array(data["test"])
+    data = request.get_json(force=True)
+    data=np.array(data["test"])
     #prediction =model.predict(data)
     #return render_template('index.html',title=int(np.argmax(prediction)))
     #return json.dumps(int(np.argmax(prediction)))
-    title = 'Good Flask'
-    return render_template('index.html',json=title)
+    #title = 'Good Flask'
+    return render_template('index.html',json=str(data))
 if __name__ == '__main__':
     app.run()
